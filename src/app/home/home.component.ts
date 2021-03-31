f53e638d204e5761fd1c2f47b50d3bee952578b0
Henrique <hmelo@cgee.org.br>
2021-03-31 01:48:45 -0300

    slide show on home page and all images uploaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images: any = ["img1","img2", "img3", "cover"];
+  i: number = 0;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    setInterval(() =>{
+      this.cImage('next')
+    },12000)
+  }
+
+  cImage(value?: string){
+    if(value==='next'){
+      if(this.i<this.images.length - 1) this.i+=1
+      else this.i = 0
+      return
+    }
+    if(this.i>0) this.i-=1
+    else this.i=this.images.length - 1
   }
 
 }
