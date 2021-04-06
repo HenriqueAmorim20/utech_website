1b9ae6c701f72cff0ce4fe0cbac6be69a7e5a1d9
Henrique <hmelo@cgee.org.br>
2021-04-06 20:40:09 -0300

    Products page done, cetegories and filters working

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-products',
@@ -7,44 +8,55 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  sort: any;
+  sort: any = 'default';
+  selected: any;
   innerWidth: any;
-  products: any = [
-    {
-      title: 'iPad Pro (2020) 11" 2ª Geração Cinza-Espacial 128GB Wi-Fi',
-      price: "6.999,00",
-      imgSrc: "ipadpro.png",
-      id: "1",
-      category: "ipad"
-    },
-    {
-      title: 'Macbook Air 13" (2020) Cinza-Espacial / Processador M1 / 8GB / 256GB SSD',
-      price: "9.099,00",
-      imgSrc: "macbookair.png",
-      id: "2",
-      category: "pc"
-    },
-    {
-      title: 'MacBook Pro 13” (2020) Space Gray Touch Bar/ID - Processador M1 / 8GB / 256GB SSD',
-      price: "11.099,00",
-      imgSrc: "macbookpro.png",
-      id: "3",
-      category: "pc"
-    },
-    {
-      title: "iPad 8˚Geração Cinza espacial 128GB Wifi",
-      price: "3599,00",
-      imgSrc: "ipad.png",
-      id: "4",
-      category: "ipad"
-    }
-  ]
+  categories: any;
+  allProducts: any;
+  products: any;
 
   constructor() { }
 
   ngOnInit(): void {
     localStorage.setItem('currentPage', 'products')
     this.innerWidth = window.innerWidth
+    try {
+      //Fazer o get das categorias e dos produtos
+      this.categories = ['PCs', 'SmartWatch', 'SmartPhone', 'Periféricos', 'Ipads', 'SSDs e HDs']
+      this.allProducts = [
+        {
+          title: 'iPad Pro (2020) 11" 2ª Geração Cinza-Espacial 128GB Wi-Fi',
+          price: 6999.00,
+          imgSrc: "ipadpro.png",
+          id: "1",
+          category: "PCs"
+        },
+        {
+          title: 'Macbook Air 13" (2020) Cinza-Espacial / Processador M1 / 8GB / 256GB SSD',
+          price: 9099.00,
+          imgSrc: "macbookair.png",
+          id: "2",
+          category: "PCs"
+        },
+        {
+          title: 'MacBook Pro 13” (2020) Space Gray Touch Bar/ID - Processador M1 / 8GB / 256GB SSD',
+          price: 11099.00,
+          imgSrc: "macbookpro.png",
+          id: "3",
+          category: "PCs"
+        },
+        {
+          title: "iPad 8˚Geração Cinza espacial 128GB Wifi",
+          price: 3599.00,
+          imgSrc: "ipad.png",
+          id: "4",
+          category: "PCs"
+        }
+      ]
+    } catch (error) {
+      console.log(error)
+    }
+    this.setCategory(this.categories[0])
   }
 
   @HostListener('window: resize', ['$event'])
@@ -52,4 +64,38 @@ export class ProductsComponent implements OnInit {
     this.innerWidth = event.target.innerWidth
   }
 
+  setCategory(value?:any){
+    if(value){
+      if(this.selected===value) return;
+      this.selected = value;
+      this.products = this.allProducts.filter( (p: any) => p.category === value)
+    }
+
+    switch (this.sort) {
+      case 'a-z':
+        this.products = this.products.sort((a: any, b: any) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()))
+        break;
+      case 'z-a':
+        this.products = this.products.sort((a: any, b: any) => a.title.toLowerCase().localeCompare(b.title.toLowerCase())).reverse()
+        break;
+      case 'priceLower':
+        this.products = this.products.sort((a: any, b: any) => {return a.price - b.price});
+        break;
+      case 'priceHigher':
+        this.products = this.products.sort((a: any, b: any) => {return b.price - a.price});
+        break;
+
+      default:
+
+    }
+
+  }
+
+  getSelected(value: any){
+    return this.selected === value;
+  }
+
+  tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    this.setCategory(this.categories[tabChangeEvent.index])
+  }
 }
